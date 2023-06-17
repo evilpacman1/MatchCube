165ce4ecb486eb120bbc5c0208d6cae6e2fcb0a2
evilpacman1 <115592480+evilpacman1@users.noreply.github.com>
2023-06-17 12:25:19 +0300

    1

--- Assets/Script/TrueOrNot.cs
+++ Assets/Script/TrueOrNot.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class TrueOrNot : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+
+    public GameObject[] answerCube;
+    public GameObject _false;
+    public GameObject falePlant;
+    public void True() 
+    { 
+        for (int i = 0; i < answerCube.Length; i++) 
+        {
+
+            answerCube[i].SetActive(true);
+
+        }
+    
+    }
+    public void falseAnswer()
     {
-        
+        StartCoroutine(WhateFall());
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+  IEnumerator WhateFall() 
+    { 
+        falePlant.SetActive(true);
+        _false.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        falePlant.SetActive(false);
+        _false.SetActive(false);
+    
     }
+
 }
